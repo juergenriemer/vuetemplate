390697e8d69a83a314d83dbc6f1ea9de15b1f984
root <root@segestria.at>
2021-06-10 09:57:21 +0200

    updated run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
 kill -9 $(ps aux | grep '[l]istle-server.js' | awk '{print $2}')
 cd /Users/juergenriemer/Projects/todo/vuetemplate/server/ 
-nodemon listle-server.js
+node listle-server.js
 #DEBUG=* nodemon listle-server.js
